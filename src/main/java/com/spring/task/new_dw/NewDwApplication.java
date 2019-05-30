b2a4c1fe220037e0fefa08371f127aaf867ed84a
Marishka <spanch1k@mail.ru>
2019-05-31 02:17:03 +0300

    Initial commit
    Add .gitignore, application.properties

---
+++ src/main/java/com/spring/task/new_dw/NewDwApplication.java
@@ -0,0 +1,13 @@
+package com.spring.task.new_dw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewDwApplication.class, args);
+    }
+
+}
